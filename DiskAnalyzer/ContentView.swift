d9f3bb6e9101d4fb8d58da01bbdd55e924e8b579
superkookai <weerawut@live.com>
2026-05-29 14:16:32 +0700

    Initial Commit

---
+++ DiskAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiskAnalyzer
+//
+//  Created by Weerawut on 29/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiskAnalyzer/DiskAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskAnalyzerApp.swift
+//  DiskAnalyzer
+//
+//  Created by Weerawut on 29/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct DiskAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
